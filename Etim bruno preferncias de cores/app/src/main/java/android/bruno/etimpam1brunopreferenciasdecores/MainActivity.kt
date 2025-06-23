5bc8f62874a28c0103fd1f818ae64223709e1c11
DEV_Romão <brunodossantosromao11@gmail.com>
2025-06-24 02:45:41 +1300

    att final
    ultima att

--- Etim bruno preferncias de cores/app/src/main/java/android/bruno/etimpam1brunopreferenciasdecores/MainActivity.kt
+++ Etim bruno preferncias de cores/app/src/main/java/android/bruno/etimpam1brunopreferenciasdecores/MainActivity.kt
@@ -1,32 +1,26 @@
-package android.raul.etimpam1raulpreferenciasdecores
+package android.bruno.etimpam1brunopreferenciasdecores
 
-import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
-import android.raul.etimpam1raulpreferenciasdecores.databinding.ActivityMainBinding
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.etimpamigustavoportelapreferenciasdecores.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var preferencias:SharedPreferences
+    private val NOME_ARQUIVO = "minhas_preferencias"
 
-    companion object{
-        const val NOME_ARQUIVO = "arquivo_prefs.xml"
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         var cor = ""
+
         binding.btnCor1.setOnClickListener {
             cor = "#C20EE4"
             salvarCor(cor)
@@ -45,58 +39,53 @@ class MainActivity : AppCompatActivity() {
         binding.btnCor4.setOnClickListener {
             cor = "#03A9F4"
             salvarCor(cor)
-
         }
 
         binding.btnCor5.setOnClickListener {
             cor = "#BC6F06"
             salvarCor(cor)
-
         }
-
-
     }
+
 
     override fun onResume() {
         super.onResume()
-
         val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
         val cor = preferencias.getString("cor", "")
-
-        if(cor!!.isNotEmpty()){
+        if (cor!!.isNotEmpty()) {
             binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
         }
+
     }
+
 
     private fun salvarCor(cor: String) {
         binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
-
-        binding.btnTrocar.setOnClickListener {view ->
+        binding.btnTrocar.setOnClickListener { view ->
             val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
             val editor = preferencias.edit()
             editor.putString("cor", cor)
-            editor.putString("nome", "Raul")
-            editor.putString("sobrenome", "Luis")
-            editor.putInt("Idade", 16)
+            editor.putString("nome", "Bruno")
+            editor.putString("sobrenome", "Romão")
+            editor.putInt("idade", 90)
             editor.apply()
 
             snackBar(view)
         }
 
+    }
+
+    private fun snackBar(view: View) {
+        val snackbar =
+            Snackbar.make(view, "cor de fundo alterada com sucesso!", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK") {
 
         }
+        snackbar.setActionTextColor(Color.BLUE)
+        snackbar.setBackgroundTint(Color.LTGRAY)
+        snackbar.setTextColor(Color.GREEN)
+        snackbar.show()
 
-        private fun snackBar(view:View){
-            val snackbar = Snackbar.make(view, "Cor de fundo alterada com sucesso", Snackbar.LENGTH_INDEFINITE)
-            snackbar.setAction("OK"){
 
-            }
-
-            snackbar.setActionTextColor(Color.YELLOW)
-            snackbar.setBackgroundTint(Color.BLUE)
-            snackbar.setTextColor(Color.RED)
-            snackbar.show()
-        }
-}
-
+    }
 
