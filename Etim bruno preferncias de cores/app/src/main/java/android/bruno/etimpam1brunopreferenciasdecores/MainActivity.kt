acc83ca4b38e029fceca63f6e922d72c5bd5b585
DEV_Romão <brunodossantosromao11@gmail.com>
2025-06-26 15:00:30 -0300

    Update MainActivity.kt

--- Etim bruno preferncias de cores/app/src/main/java/android/bruno/etimpam1brunopreferenciasdecores/MainActivity.kt
+++ Etim bruno preferncias de cores/app/src/main/java/android/bruno/etimpam1brunopreferenciasdecores/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.etimpamigustavoportelapreferenciasdecores.databinding.ActivityMainBinding
+import com.example.etimpam1brunopreferenciasdecores.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
